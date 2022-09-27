a7785bd0fd56a150428ee3ce537e78b9a25ec2a3
Arthur Werle <arthur.werle@gmail.com>
2022-09-27 19:55:24 -0300

    add confirm email endpoint

--- src/functions/auth.ts
+++ src/functions/auth.ts
@@ -1,6 +1,7 @@
 import type { Handler, APIGatewayEvent } from 'aws-lambda'
 import { emailRegex, passwordRegex } from '../constants/Regexes'
 import { CognitoServices } from '../services/CognitoService'
+import { ConfirmEmailRequest } from '../types/auth/ConfirmEmailRequest'
 import { UserRegisterRequest } from '../types/auth/UserRegisterRequest'
 import { DefaultJsonResponse, formatResponse } from '../utils/formatResponse'
 
@@ -8,23 +9,47 @@ export const register: Handler = async(event: APIGatewayEvent): Promise<DefaultJ
   try {
     const { USER_POOL_ID, USER_POOL_CLIENT_ID } = process.env
 
-    if (!USER_POOL_ID || !USER_POOL_CLIENT_ID) return formatResponse(500, 'ENVs do Cognito não encontradas.')
+    if (!USER_POOL_ID || !USER_POOL_CLIENT_ID) return formatResponse(500, 'Cognito ENV variables not found.')
 
-    if (!event.body) return formatResponse(400, 'Parâmetros de entrada inválidos.')
+    if (!event.body) return formatResponse(400, 'Missing request body.')
 
     const request = JSON.parse(event.body) as UserRegisterRequest
     const { name, password, email } = request
 
-    if (!email || !email.match(emailRegex)) return formatResponse(400, 'Email inválido.')
-    if (!password || !password.match(passwordRegex)) return formatResponse(400, 'Senha inválida.')
-    if (!name || name.trim().length < 2) return formatResponse(400, 'Nome inválido.')
+    if (!email || !email.match(emailRegex)) return formatResponse(400, 'Invalid email.')
+    if (!password || !password.match(passwordRegex)) return formatResponse(400, 'Invalid password.')
+    if (!name || name.trim().length < 2) return formatResponse(400, 'Invalid name.')
 
     await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).signUp(email, password)
 
-    return formatResponse(200, 'Usuário cadastrado com sucesso.')
+    return formatResponse(200, 'User created.')
 
   } catch(error) {
-    console.log('Error on user register:', error)
-    return formatResponse(500, 'Erro ao cadastrar usuário, tente novamente.')
+    console.log('Error creating user:', error)
+    return formatResponse(500, 'Error on creating user, please try again.')
+  }
+}
+
+export const confirmEmail: Handler = async(event: APIGatewayEvent): Promise<DefaultJsonResponse> => {
+  try {
+    const { USER_POOL_ID, USER_POOL_CLIENT_ID } = process.env
+
+    if (!USER_POOL_ID || !USER_POOL_CLIENT_ID) return formatResponse(500, 'Cognito ENV variables not found.')
+
+    if (!event.body) return formatResponse(400, 'Missing request body.')
+
+    const request = JSON.parse(event.body) as ConfirmEmailRequest
+    const { email, verificationCode } = request
+
+    if (!email || !email.match(emailRegex)) return formatResponse(400, 'Invalid email.')
+    if (!verificationCode || verificationCode.length !== 6) return formatResponse(400, 'Invalid verification code.')
+    
+    await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).confirmEmail(email, verificationCode)
+
+    return formatResponse(200, 'User verified.') 
+
+  } catch(error) {
+    console.log('Error confirming user:', error)
+    return formatResponse(500, 'Error confirming user, please try again.')
   }
 }
--- src/services/CognitoService.ts
+++ src/services/CognitoService.ts
@@ -1,4 +1,4 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js"
+import { CognitoUser, CognitoUserPool } from "amazon-cognito-identity-js"
 
 export class CognitoServices {
   constructor(
@@ -6,17 +6,40 @@ export class CognitoServices {
     private userPoolCLient: string
   ){}
 
+  private poolData = {
+    UserPoolId: this.userPoolId,
+    ClientId: this.userPoolCLient
+  }
+
   public signUp(email: string, password: string): Promise<any> {
     return new Promise<any>((resolve, reject) => {
       try {
-        const poolData = {
-          UserPoolId: this.userPoolId,
-          ClientId: this.userPoolCLient
-        }
-
-        const userPool = new CognitoUserPool(poolData)
+        const userPool = new CognitoUserPool(this.poolData)
         userPool.signUp(email, password, [], [], (err, result) => {
           if(err) return reject(err)
+
+          resolve(result)
+        })
+      } catch(error) {
+        reject(error)
+      }
+    })
+  }
+
+  public confirmEmail(email: string, verificationCode: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      try {
+        const userPool = new CognitoUserPool(this.poolData)
+
+        const userData = {
+          Username: email,
+          Pool: userPool
+        }
+
+        const user = new CognitoUser(userData)
+
+        user.confirmRegistration(verificationCode, true, (err, result) => {
+          if (err) return reject(err)
 
           resolve(result)
         })
