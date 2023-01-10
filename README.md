83f5be174ff2ece676f2e508685d28d5e5450465
Arthur Werle <arthur.werle@gmail.com>
2023-01-10 07:35:35 -0300

    add winston to log manaagment

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # Devagram
+
 ## Technologies used
- - Serverless framework
- - Node
- - Typescript
- - AWS
-  - DynamoDB, S3, Lambda
- 
+
+- Serverless framework
+- Node
+- Typescript
+- AWS
+- DynamoDB, S3, Lambda
+
 ## Commands
- - `sls deploy` to deploy changes to AWS and get the list of endpoints created.
+
+- `sls deploy` to deploy changes to AWS and get the list of endpoints created.
 
 ## API
- - endpoints are created in `src/functions` folder. each function, in each file, is a different endpoint, which builds and url `{random-url-generated-by-aws}/{function-name}`.
- - the api configuration is on `serverless.yml` file.
+
+- endpoints are created in `src/functions` folder. each function, in each file, is a different endpoint, which builds and url `{random-url-generated-by-aws}/{function-name}`.
+- the api configuration is on `serverless.yml` file.
 
 ## Testing the API
- - I'm using Hoppscotch to test the API, making simple requests adding data manually.
+
+- I'm using [Hoppscotch](https://hoppscotch.io/) to test the API, making simple requests adding data manually.
+
+## Logs
+
+- I'm using [Winston](https://github.com/winstonjs/winston/tree/2.x) to manage logs with logs levels. As you can see in `serverless.yml` file, there is a cli param called level (`--param="level={levelName}"`) where you can inform which log level you want, setting an ENV variable. So `info` and `debug` logs will only display if the ENV variable is set to do so, making the logs organized by the log levels you want.
--- src/functions/login.ts
+++ src/functions/login.ts
@@ -3,11 +3,15 @@ import { CognitoServices } from "../services/CognitoService"
 import { LoginRequest } from "../types/login/LoginRequest"
 import { validateEnvVariables } from "../utils/environment"
 import { DefaultResponse, formatResponse } from "../utils/formatResponse"
+import { logger } from '../utils/logger'
 
 export const handler: Handler = async(event: APIGatewayEvent): Promise<DefaultResponse> => {
     try {
       const { USER_POOL_ID = '', USER_POOL_CLIENT_ID = '', error } = validateEnvVariables(['USER_POOL_ID', 'USER_POOL_CLIENT_ID'])
-      if(error) return formatResponse(500, error)
+      if(error) {
+        logger.error('login.handler: ', error)
+        return formatResponse(500, error)
+      }
   
       if (!event.body) return formatResponse(400, 'Missing request body.')
   
@@ -16,12 +20,15 @@ export const handler: Handler = async(event: APIGatewayEvent): Promise<DefaultRe
   
       if (!login || !password) return formatResponse(400, 'Missing login or password.')
       
+      logger.info('login.handler start: ', login)
       const result = await new CognitoServices(USER_POOL_ID, USER_POOL_CLIENT_ID).login(login, password)
+      logger.debug('login.handler cognito response: ', result)
+      logger.info('login.handler finish: ', login)
   
       return formatResponse(200, undefined, result) 
   
     } catch(error) {
-      console.log('Error on login:', error)
+      logger.error('login.handler: Error on login:', error)
       return formatResponse(500, 'Error on login, please try again.')
     }
   }
